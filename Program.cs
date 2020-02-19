e9977b0f1438fed2377afb4554abae13f899988f
Michael Barry <michael.barry@gmo.com>
2020-02-19 15:09:18 -0500

    Demo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bugdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
